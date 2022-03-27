db63a8021f7201f08b777994b7ae38ec766f1b1d
juliajulia89 <julkajanicka@gmail.com>
2022-03-27 16:11:27 +0200

    sidebar sorted

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,18 +1,25 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
 
 export const SidebarData = [
   {
     title: "Home",
     path: "/",
-    className: "nav-text",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
   },
   {
     title: "Flights",
     path: "/flights",
-    className: "nav-text",
+    icon: <IoIcons.IoIosPaperPlane />,
+    cName: "nav-text",
   },
   {
     title: "Comment",
     path: "/comment",
-    className: "nav-text",
-  }
+    icon: <FaIcons.FaRegComments />,
+    cName: "nav-text",
+  },
 ];
--- src/pages/Flights.js
+++ src/pages/Flights.js
@@ -4,6 +4,7 @@ function Flights() {
   return (
     <div className="flights">
       <h1>flights</h1>
+      <img src="/assets/abstract-bg.jpeg" alt="plane"/>
     </div>
   );
 }
