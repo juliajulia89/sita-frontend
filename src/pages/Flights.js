447589a44895ff7cc9fe59433939c0fba5e4692a
juliajulia89 <julkajanicka@gmail.com>
2022-03-27 23:20:00 +0200

    added navbar

--- src/pages/Flights.js
+++ src/pages/Flights.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 function Flights() {
   return (
+    <>
+    <Navbar />
     <div className="flights">
       <h1>flights</h1>
       <img src="/assets/abstract-bg.jpeg" alt="plane"/>
     </div>
+    </>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Logo from "../assets/logo.png";
-
+import Navbar from "../components/Navbar";
 
 function Home() {
   return (
+    <>
+     <Navbar />
     <div className="home">
       <h1>Welcome to SITA </h1>
       <img className="logo" src={Logo} alt="sita logo" />
     </div>
+    </>
   );
 }
 
