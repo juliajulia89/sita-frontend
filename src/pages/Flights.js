a233fb6487ece118afb026c9977a5898ac068d3b
juliajulia89 <julkajanicka@gmail.com>
2022-03-28 03:16:55 +0200

    spinner added

--- src/pages/Flights.js
+++ src/pages/Flights.js
@@ -1,15 +1,41 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import "../index.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Spinner from "../assets/running-man.gif";
+import "../index.css";
 
-function Flights() {
+function Flights(props) {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const API_URI = process.env.REACT_APP_API_URI;
+
+  useEffect(() => {
+    axios
+      .get(`${API_URI}/flights`)
+      .then((response) => {
+        setData(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <>
-    <Navbar />
-    <div className="flights">
-      <h1>flights</h1>
-  
-    </div>
+      {isLoading ? (
+        <>
+          <img src={Spinner} alt="loading..." className="spinner" />
+        </>
+      ) : (
+        <>
+          <Navbar />
+          <div className="flights-list">
+            
+          </div>
+        </>
+      )}
     </>
   );
 }
